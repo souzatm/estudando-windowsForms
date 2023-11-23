6f91504b92c1418dd486944bae1b1136976c27e7
Thiago Souza <tsouzatm@outlook.com>
2023-11-23 09:26:32 -0300

    Nova branch - Reorganizando o projeto

---
+++ CursoWindowsForms/Formulários Curso 1/Frm_ValidaCPF2.cs
@@ -0,0 +1,68 @@
+﻿using CursoWindowsFormsBiblioteca;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF2 : Form
+    {
+        public Frm_ValidaCPF2()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            string verConteudo;
+            verConteudo = Msk_CPF.Text;
+            verConteudo = verConteudo.Replace(".", "").Replace("-", "");
+            verConteudo = verConteudo.Trim();
+
+            if(verConteudo == "")
+            {
+                MessageBox.Show("Você deve digitar um CPF", "Mensagem de Validação", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (verConteudo.Length != 11)
+                {
+                    MessageBox.Show("CPF deve conter 11 dígitos", "Mensagem de Validação", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                }
+                else
+                {
+                    if (MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        bool validaCPF = false;
+                        validaCPF = Cls_Uteis.Valida(Msk_CPF.Text);
+                        if (validaCPF == true)
+                        {
+                            MessageBox.Show("CPF VÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("CPF INVÁLIDO", "Mensagem de Validação", MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ CursoWindowsForms/Formulários Curso 1/Frm_ValidaSenha.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static CursoWindowsFormsBiblioteca.Cls_Uteis;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaSenha : Form
+    {
+        bool VerSenhaTxt = false;
+
+        public Frm_ValidaSenha()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Senha.Text = "";
+            Lbl_Resultado.Text = "";
+        }
+
+        private void Txt_Senha_KeyDown(object sender, KeyEventArgs e)
+        {
+            ChecaForcaSenha verificaSenha = new ChecaForcaSenha();
+            ChecaForcaSenha.ForcaDaSenha forca;
+            forca = verificaSenha.GetForcaDaSenha(Txt_Senha.Text);
+            Lbl_Resultado.Text = forca.ToString();
+
+            if(Lbl_Resultado.Text == "Inaceitavel" | Lbl_Resultado.Text == "Fraca")
+            {
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+            else if (Lbl_Resultado.Text == "Aceitavel")
+            {
+                Lbl_Resultado.ForeColor = Color.Blue;
+            }
+            else
+            {
+                Lbl_Resultado.ForeColor = Color.Green;
+            }
+        }
+
+        private void Btn_VerSenha_Click(object sender, EventArgs e)
+        {
+            if(VerSenhaTxt == false)
+            {
+                Txt_Senha.PasswordChar = '\0';
+                VerSenhaTxt = true;
+                Btn_VerSenha.Text = "Esconder senha";
+            }
+            else
+            {
+                Txt_Senha.PasswordChar = '*';
+                VerSenhaTxt = false;
+                Btn_VerSenha.Text = "Ver senha";
+            }
+        }
+    }
+}
